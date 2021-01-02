835635d49b9b6c13967c29eb4629ff08ba72e360
cecep31 <cecepjanuardi31@gmail.com>
2021-01-02 12:26:30 +0700

    add models router

--- router/models.py
+++ router/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Mrouter(models.Model):
+    nama = models.CharField(max_length=40)
+    host = models.CharField(max_length=40)
+    user = models.CharField(max_length=40)
+    password = models.CharField(max_length=50)
+    up_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.nama
+    
+class Router(models.Model):
+    nama = models.CharField(max_length=40)
+    host = models.CharField(max_length=40)
+    user = models.CharField(max_length=40)
+    password = models.CharField(max_length=50)
+    up_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.nama
+
+
